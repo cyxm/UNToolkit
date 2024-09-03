125b9c49451e137439db15fe1346585306e3d713
ClarkXiang <ClarkXiang@paxsz.com>
2024-09-03 18:09:47 +0800

    添加mui

--- app/app-ui/src/page/Home.js
+++ app/app-ui/src/page/Home.js
@@ -1,9 +1,11 @@
 import Splitter, { SplitDirection } from "@devbookhq/splitter"
+import Button from '@mui/material/Button';
+
 export default function Home() {
     return (
         <Splitter direction={SplitDirection.Horizontal} minWidths={200}>
             <div>234</div>
-            <div>4123</div>
+            <Button variant="contained">Hello world</Button>
         </Splitter>
     );
 }
