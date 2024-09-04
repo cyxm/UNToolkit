e9bd68786b811e6018f83f7c412fc5f478dc53fd
ClarkXiang <ClarkXiang@paxsz.com>
2024-09-04 18:51:53 +0800

    实现全屏,添加树

--- app/app-ui/src/page/Home.js
+++ app/app-ui/src/page/Home.js
@@ -1,10 +1,31 @@
 import Splitter, { SplitDirection } from "@devbookhq/splitter"
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
 export default function Home() {
     return (
-        <Splitter direction={SplitDirection.Horizontal} minWidths={200}>
-            <div>234</div>
+        <Splitter direction={SplitDirection.Horizontal} minWidths={[200, 250]}>
+            <Box>
+                <SimpleTreeView>
+                    <TreeItem itemId="grid" label="Data Grid">
+                        <TreeItem itemId="grid-community" label="@mui/x-data-grid" />
+                        <TreeItem itemId="grid-pro" label="@mui/x-data-grid-pro" />
+                        <TreeItem itemId="grid-premium" label="@mui/x-data-grid-premium" />
+                    </TreeItem>
+                    <TreeItem itemId="pickers" label="Date and Time Pickers">
+                        <TreeItem itemId="pickers-community" label="@mui/x-date-pickers" />
+                        <TreeItem itemId="pickers-pro" label="@mui/x-date-pickers-pro" />
+                    </TreeItem>
+                    <TreeItem itemId="charts" label="Charts">
+                        <TreeItem itemId="charts-community" label="@mui/x-charts" />
+                    </TreeItem>
+                    <TreeItem itemId="tree-view" label="Tree View">
+                        <TreeItem itemId="tree-view-community" label="@mui/x-tree-view" />
+                    </TreeItem>
+                </SimpleTreeView>
+            </Box>
             <Button variant="contained">Hello world</Button>
         </Splitter>
     );
