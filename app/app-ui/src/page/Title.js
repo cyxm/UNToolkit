a4d45bddbfbed853b85facb5b2c0e5368e346485
ClarkXiang <ClarkXiang@paxsz.com>
2024-10-11 15:29:37 +0800

    加入图标库,更改标题栏

--- app/app-ui/src/page/Title.js
+++ app/app-ui/src/page/Title.js
@@ -1,10 +1,12 @@
 import React from "react"
-import { Grid2 } from "@mui/material";
+import { Stack } from "@mui/material";
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
 
 export default function Title() {
     return (
-        <Grid2 size={12}  backgroundColor={"#f555f5"}>
-            rrr4123
-        </Grid2>
+        <Stack direction="row" backgroundColor={"#f555f5"}>
+            <DeleteSharpIcon />
+            <h1>Title</h1>
+        </Stack>
     )
 }
