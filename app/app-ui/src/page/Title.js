c8bb7fb354fe3ad4ead8512ebb1348511d084e81
ClarkXiang <ClarkXiang@paxsz.com>
2024-10-21 19:21:10 +0800

    添加button

--- app/app-ui/src/page/Title.js
+++ app/app-ui/src/page/Title.js
@@ -1,18 +1,18 @@
 import React from "react"
 import '../styles.css';
-import { Stack } from "@mui/material";
+import { Stack, SvgIcon } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import CropSquareIcon from '@mui/icons-material/CropSquare';
 import MinimizeIcon from '@mui/icons-material/Minimize';
 import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService';
-import IconButton from "../widget/icon/IconButton";
+import Button from '@mui/material/Button';
 
 export default function Title({ styleName }) {
     return (
-        <Stack direction="row" className={styleName}>
-            <IconButton styleName="test">
-                <HomeRepairServiceIcon />
-            </IconButton>
+        <Stack direction="row" className={styleName} display={"flex"} justifyContent={"flex-end"}>
+            <Button style={{ height: "inherit", width:0,paddingLeft:"50%"}}>
+                <HomeRepairServiceIcon fontSize="medium" />
+            </Button>
             <MinimizeIcon />
             <CropSquareIcon />
             <CloseIcon />
