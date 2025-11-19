b8c40cb5cf27f12eb76ec3f4d1fedcb7fb4f66c2
clark-pax <clarkxiang@pax.us>
2025-11-19 19:20:27 +0800

    删除json工具

--- tool/app-python/logic/json/AutoCallJsonEncoder.py
+++
@@ -1,9 +0,0 @@
-# encoding=UTF-8
-import json
-
-
-class AutoCallJsonEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if hasattr(obj, "__json_encode__") and callable(obj.__json_encode__):
-            return obj.__json_encode__()
-        return super().default(obj)
